84ba42b71741f480e743ba797aadc084bceed980
Philippe Lenoir <philippe.lenoir@prologism.fr>
2021-11-09 11:38:29 +0000

    ajout chiffrement

--- fr_metropolis.py
+++ fr_metropolis.py
@@ -4,6 +4,7 @@ import logging
 import re
 import os
 import numpy as np
+import random as rd
 import codecs
 import pickle
 
@@ -43,7 +44,7 @@ def charge_dico():
                 
         fr_dico = simplifie(fr_dict)
         # Ajout des lettres et de qu'
-        fr_dico = ['QU'] + ['A'] + ['C'] + ['L'] + ['M'] + ['N'] + ['O'] + ['S'] + ['T'] + ['Y'] +fr_dico.split(" ")
+        fr_dico = ['QU'] + ['A'] + ['C'] + ['J'] + ['L'] + ['M'] + ['N'] + ['O'] + ['S'] + ['T'] + ['Y'] +fr_dico.split(" ")
         pickle.dump(fr_dico, open( fr_dat, "wb" ))
 
     cpt_mots = len(fr_dico)
@@ -251,6 +252,8 @@ def plausibilite(texte):
 
         La plausibilite c'est la somme des probabilités de rencontrer un bigramme 
         multipliée par le nombre de fois qu'on le rencontre.
+
+        Dans l'algorithme de MH, plausibilite = f, fonction proportionnelle à pi
     """
     epsilon = 10e-7
     plau = 0
@@ -269,6 +272,7 @@ def plausibilite(texte):
     logging.debug("Plausibilité du texte : %f", plau)
     return plau
 
+
 def score(texte):
     """
         Evalue le score de la proposition de déchiffrement en repérant les mots
@@ -286,7 +290,8 @@ def score(texte):
         lettres_tot += len(mot)
     return lettres_ok/lettres_tot    
 
-def dechiffrer(texte, code):
+
+def dechiffre(texte, code):
     """
         Prend un texte chiffré en argument et renvoie une proposition de 
         dechiffrement basée sur la fréquence des caractères.
@@ -306,21 +311,48 @@ def dechiffrer(texte, code):
     return res
 
 
+def chiffre(texte):
+    """
+        Chiffrement d'un texte avec un code tiré au hasard
+    """
+    code = rd.sample(alphabet, 26)
+    simple = simplifie(texte)
+    return dechiffre(simple, code)
+
 
 def echange(clef):
     """
         échange 2 lettres de la clef
+
+        Dans l'algorithme de MH, echange est la fonction de tirage aléatoire
+        qui ne dépend que de l'état précédent.
         --
         retourne    : nouvelle clef
     """
     i = np.random.randint(0,26)
     j = np.random.randint(0,26)
+
+    a,b = rd.choices(range(0,26), weights=list(fkv_ref.values()), k=2)
     while i == j:
         j = np.random.randint(0,26)
 
-    clef[i], clef[j] = clef[j], clef[i]
+    #clef[i], clef[j] = clef[j], clef[i]
+    clef[a], clef[b] = clef[b], clef[a]
     return clef
 
+def acceptation(cur, new):
+    """
+        Calcule le taux d'acceptation alpha et le compare à une variable aléatoire x dans [O;1[:
+            Si x <= alpha, alors retourne True accepter (donc si alpha > 1}, on accepte nécessairement)
+            Si x > alpha, alors retourne False
+        --
+        REFERENCE : https://fr.wikipedia.org/wiki/Algorithme_de_Metropolis-Hastings
+    """
+    x = rd.random()
+    #if x <= np.exp(new - cur) :
+    if x <=  (cur / new) * 0.001 :
+        return True
+    return False
 
 def metropolis(max_iter, texte_init):
     """
@@ -328,8 +360,8 @@ def metropolis(max_iter, texte_init):
         --
         retourne    : proposition de déchiffrement
     """
-    break_plau  = -1.6          # max plausibilité calculée à partir de laquelle on estime le résultat juste
-    chance      = 0.05          # chance d'accépter un code moins bon pour s'éloigner d'un possible maximum local
+    break_plau = -1.6          # max plausibilité calculée à partir de laquelle on estime le résultat juste
+    score_mots = 0
 
     # le premier code est calculé sur la base des fréquences de référence
     global fk_ref 
@@ -340,16 +372,17 @@ def metropolis(max_iter, texte_init):
         ind = char_to_id(c)
         cur_code[ind] = fk_ref[i]
         logging.debug("remplace %s (%d) par %s", c, ind ,fk_ref[i] )
+    cur_texte = dechiffre(texte_init, cur_code)
+    # score_mots = score(cur_texte)
+    # cur_plau = 4*score_mots + plausibilite(cur_texte)
+    cur_plau = plausibilite(cur_texte)
+    logging.info("Code initial (p=%f) %s", cur_plau, cur_code)
+    logging.info("Proposition initiale [%s]", cur_texte)
 
-    logging.info("code init %s", cur_code)
+    best_text  = cur_texte
+    best_plau  = cur_plau                
+    best_code  = cur_code
 
-    cur_texte   = dechiffrer(texte_init, cur_code)
-    cur_plau    = plausibilite(cur_texte)
-
-    best_code   = cur_code
-    best_text   = cur_texte
-    best_plau   = cur_plau
-    
     cpt=0
     while cpt < max_iter :
         cpt+=1 
@@ -359,42 +392,24 @@ def metropolis(max_iter, texte_init):
 
         # échange de 2 lettres de la clef       
         new_code  = echange(cur_code)
-        new_texte = dechiffrer(cur_texte, new_code)
+        new_texte = dechiffre(cur_texte, new_code)
+        # score_mots = score(new_texte)
+        # new_plau  = 4*score_mots + plausibilite(new_texte)
         new_plau  = plausibilite(new_texte)
-        
-        x = np.random.rand()
-        # si la plausibilité est meilleure on garde le nouveau texte,
-        # sinon soit on garde le nouveau texte, soit on revient au texte précédent.
-        # Dépend du résultat d'un aléa:
-        # TODO expliquer l'aléa
-        if new_plau > cur_plau:
+
+        if new_plau > best_plau:
+            logging.info("(itération %d) Meilleure plausibilité %f", cpt, new_plau)
+            best_text  = new_texte
+            best_plau  = new_plau                
+            best_code  = new_code     
+
+        # Metropolis-Hastings (cf fonction d'acceptation)
+        if acceptation(new_plau, cur_plau):
             cur_plau  = new_plau
             cur_texte = new_texte
             cur_code  = new_code
-            if new_plau > best_plau:
-                logging.info("(itération %d) Meilleure plausibilité %f", cpt, new_plau)
-                best_text  = new_texte
-                best_plau  = new_plau                
-                best_code  = new_code
-        else:
-            # le facteur chance est d'autant plus petit qu'on avance dans les itérations
-            #chance = chance * 0.99
 
-            # plus l'écart est important, moins on a de chance d'accepter
-            # cur_score = score(cur_texte)
-            # p = np.exp((new_plau - cur_plau) - cur_score)
-            logging.info("x %f, coeff %f", x, np.exp(new_plau - cur_plau))
-            if (x < np.exp(new_plau - cur_plau)):
-            #if ( (cur_plau / new_plau) * 0.001) > x :
-                logging.info("(itération %d) Régression plausibilité %f -> %f", cpt, cur_plau, new_plau)
-                cur_texte = new_texte
-                cur_plau  = new_plau
-                cur_code  = new_code                
-     
-        logging.debug("Proposition %d(%f) [%s]", cpt, cur_plau, cur_texte)
-
-    logging.info("Proposition finale(%f) [%s]", best_plau, best_text)
-    return best_code
+    return best_code, best_plau, best_text
 
 
 # Point d'entrée du programme, utilisation depuis la ligne de commande:
@@ -427,6 +442,7 @@ fk_ref  = list(fkv_ref.keys())
 big_ref = bigramme(baba)
 ic_ref = indice(baba)
 p = plausibilite(baba)
+logging.info("Plausibilité des Misérables (p=%f)", p)
 
 """
     Analyse d'une expression française pour vérifier la validité des fonctions d'analyse
@@ -436,7 +452,7 @@ print("Analyse de [%s]", expr_fr)
 baba = simplifie(expr_fr)
 ic = indice(baba)
 p = plausibilite(baba)
-
+logging.info("Plausibilité de expr_fr (p=%f)", p)
 """
     Analyse d'une expression anglaise pour vérifier la non validité des fonctions d'analyse dans ce cas
 """
@@ -445,21 +461,32 @@ logging.info("Analyse de [%s]", expr_en)
 baba = simplifie(expr_en)
 ic = indice(baba)
 p = plausibilite(baba)
+logging.info("Plausibilité de expr_en (p=%f)", p)
 
 """
     Déchiffrement d'un texte codé par substitution.
-
-    Une première proposition est éléborée à partir du pct d'occurence des
-    lettres dans le texte chiffré qui sont comparées au pct de ref. Une 
-    première substitution est appliquée.
-
-    Cette proposition initiale est ensuite utilisée comme base de l'algorithme
-    de Monte-Carlo que l'on va itérer un certain nombre de fois.
 """
-#enigme ='Gf yaom wf htmom tllao daol lwk wf mtvmt hswl sgfu eak s wmosolamogf r wf mtvmt rt mkgol dgml ft ltdzst hal ygfemogfftk assgfl hswl sgfu hgwk xgok pwljw gw ea xa'
-enigme = """NASJX OXH NXH SOE BXYA SJXEA PY SNXYZEA ! YH ZASJSEG BSP ZAEJESG, RSA G SP ZY JY ? OS BAXBXPEZEXH EHRGYZ GS DEPBSAEZEXH, P'SKKASHRUEPPSHZ D'YH SZZAENYZ BSAOE JEHTZ-PEM. BSP RXH ! (XYE C'SE KSEZ KXAZ) 
-            JXEGS QYE PSHP PXYBRXH PSYAS ASJEA ZXH RENXYGXZ, XY DY OXEHP ZXH SAZ DY BLZUXH. SY BGSEPEA D'YH CXYA HXYP JXEA SYZXYA D'YH NXRV XY DY KGSRXH D'YH JEH, QY'EG PXEZ NGSHR XY AYNEP. ZXH JEG SOE"""
+emile = """La nature veut que les enfants soient enfants avant que d’être hommes. Si nous voulons 
+            pervertir cet ordre, nous produirons des fruits précoces, qui n’auront ni maturité ni saveur, 
+            et ne tarderont pas à se corrompre ; nous aurons de jeunes docteurs et de vieux enfants. 
+            L’enfance a des manières de voir, de penser, de sentir, qui lui sont propres ; rien n’est moins 
+            sensé que d’y vouloir substituer les nôtres ; et j’aimerais autant exiger qu’enfant eût cinq 
+            pieds de haut, que du jugement à dix ans. En effet, à quoi lui servirait la raison à cet âge ? 
+            Elle est le frein de la force, et l’enfant n’a pas besoin de ce frein."""
 
-logging.info("Déchiffrement de [%s]", enigme)
-baba = simplifie(enigme)
-metropolis(100, baba)
+enigme = chiffre(emile)
+logging.info("---- Déchiffrement d'un extrait d'Emile de JJ Rousseau")
+code, p, solution = metropolis(100000, enigme)
+logging.info("Meilleur code trouvé (p=%f) %s", p, code)
+logging.info("Proposition finale [%s]", solution)
+
+# enigme = """NASJX OXH NXH SOE BXYA SJXEA PY SNXYZEA ! YH ZASJSEG BSP ZAEJESG, RSA G SP ZY JY ? OS BAXBXPEZEXH 
+#             EHRGYZ GS DEPBSAEZEXH, P'SKKASHRUEPPSHZ D'YH SZZAENYZ BSAOE JEHTZ-PEM. BSP RXH ! (XYE C'SE KSEZ KXAZ) 
+#             JXEGS QYE PSHP PXYBRXH PSYAS ASJEA ZXH RENXYGXZ, XY DY OXEHP ZXH SAZ DY BLZUXH. SY BGSEPEA D'YH 
+#             CXYA HXYP JXEA SYZXYA D'YH NXRV XY DY KGSRXH D'YH JEH, QY'EG PXEZ NGSHR XY AYNEP. ZXH JEG SOE"""
+
+# logging.info("---- Déchiffrement d'un enigme")
+# baba = simplifie(enigme)
+# code, p, solution = metropolis(100, baba)
+# logging.info("Meilleur code trouvé (p=%f) %s", p, code)
+# logging.info("Proposition finale [%s]", solution)
